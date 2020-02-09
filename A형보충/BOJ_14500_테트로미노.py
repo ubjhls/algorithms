b8c83338473e0186ddd6079e00446fd65ec922f9
김성훈 <ubjhls@naver.com>
2020-02-09 16:50:06 +0900

    테트로미노

---
+++ A형보충/BOJ_14500_테트로미노.py
@@ -0,0 +1,51 @@
+def dfs(x, y, count, value):
+    global ans
+    for k in range(4):
+        nx, ny = x + dx[k], y + dy[k]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
+        if not visit[nx][ny]:
+            visit[nx][ny] = True
+            if count == 4:
+                if value > ans:
+                    ans = value
+                return
+            dfs(nx, ny, count + 1, value + arr[nx][ny])
+            visit[nx][ny] = False
+
+def mid(x, y):
+    global value, ans
+    if x-1 >=0 and x+1 < n and y+1 < m:
+        tmp = 0
+        tmp = value + arr[x-1][y] + arr[x+1][y] + arr[x][y+1]
+        if tmp > ans:
+            ans = tmp
+    if x-1 >=0 and x+1 < n and y-1 >= 0:
+        tmp = 0
+        tmp = value + arr[x - 1][y] + arr[x + 1][y] + arr[x][y - 1]
+        if tmp > ans:
+            ans = tmp
+    if y+1 <m and y-1 >=0 and x+1 <n:
+        tmp = 0
+        tmp = value + arr[x][y + 1] + arr[x][y - 1] + arr[x + 1][y]
+        if tmp > ans:
+            ans = tmp
+    if y+1 <m and y-1 >=0 and x-1>=0:
+        tmp = 0
+        tmp = value + arr[x][y + 1] + arr[x][y - 1] + arr[x - 1][y]
+        if tmp > ans:
+            ans = tmp
+
+n, m = map(int, input().split())
+dx, dy = (0, 0, 1, -1), (1, -1, 0, 0)
+arr = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+
+for i in range(n):
+    for j in range(m):
+        visit = [[False] * m for _ in range(n)]
+        count = 1
+        value = arr[i][j]
+        mid(i, j)
+        dfs(i, j, count, value)
+print(ans)
