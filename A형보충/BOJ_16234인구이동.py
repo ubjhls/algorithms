8da6ac548f9f9ae85e6786447a749fa45b1201fb
김성훈 <ubjhls@naver.com>
2020-02-28 19:22:21 +0900

    BOJ_16234_인구이동

--- A형보충/BOJ_16234인구이동.py
+++ A형보충/BOJ_16234인구이동.py
@@ -1,46 +1,50 @@
-def bfs(x, y):
-    global count
-    q.append((x, y))
-    tmp.append(arr[x][y])
+import sys, collections
+
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
+
+def bfs(v):
+    q = collections.deque()
+    point = [v]
+    level = 0
+    f_p = v[:]
+    visited[v[0]][v[1]] = True
+    q.append(v)
+
     while q:
-        x, y = q.pop(0)
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if not visit[nx][ny] and l <= abs(arr[x][y] - arr[nx][ny]) <= r:
-                visit[x][y] = True
-                visit[nx][ny] = True
-                q.append((nx, ny))
-                tmp.append(arr[nx][ny])
-                count = 1
+        level += 1
+        for _ in range(len(q)):
+            v = q.popleft()
+            for a in range(4):
+                i = v[0] + di[a]
+                j = v[1] + dj[a]
+                if 0 <= i <= N-1 and 0 <= j <= N-1 and not(visited[i][j]):
+                    if L <= abs(land[v[0]][v[1]]-land[i][j]) <= R:
+                        point.append([i, j])
+                        q.append([i, j])
+                        visited[i][j] = True
+    if level == 1:
+        visited[f_p[0]][f_p[1]] = False
+    else:
+        total_point.append(point)
 
-n, l, r = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-result = []
-q = []
+N, L, R = map(int, input().split())
+land = [list(map(int, input().split())) for _ in range(N)]
+t = 0
 while True:
-    visit = [[False] * n for _ in range(n)]
-    for w in range(n):
-        for t in range(n):
-            count = 0
-            tmp = []
-            if visit[w][t] == True:
-                continue
-            bfs(w, t)
-            if count == 0:
-                continue
-            else:
-                result.append(count)
-                avg = sum(tmp) // len(tmp)
-                for j in range(n):
-                    for k in range(n):
-                        if visit[j][k]:
-                            arr[j][k] = avg
-
-    if count == 0:
+    total_point = []
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if not(visited[i][j]):
+                bfs([i, j])
+    if not(total_point):
         break
-
-print(sum(result))
+    else:
+        # total_point에 들어가있는 좌표들의 값을 이용해 해당 좌표의 land값들의 평균을 내고 좌표에 입력한다.
+        for points in total_point:
+            result = sum(map(lambda x: land[x[0]][x[1]], points)) // len(points)
+            for p in points:
+                land[p[0]][p[1]] = result
+        t += 1
+print(t)
